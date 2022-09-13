285cd837b27c424b1ff221a4216f587a6ef784b2
maobao <maobao@duiba.com.cn>
2022-09-13 16:22:40 +0800

    feat: 更新eslint自定义presets包

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('eslint-define-config')
 
 module.exports = defineConfig({
   root: true,
-  // TODO: 整理自己的eslint配置规范,然后直接引用自己的npm包
-  extends: ['@sxzz/eslint-config-ts', '@sxzz/eslint-config-prettier'],
+  // 自定义eslint preset扩展包
+  extends: ['@fe92star/eslint-config-ts', '@fe92star/eslint-config-prettier'],
 })
